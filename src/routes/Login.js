86c1df3f5b11cbd6fb3815768493c7c5823add4b
Rodrigo Gastaud <rodrigogastaud_97@hotmail.com>
2022-02-21 01:16:44 -0300

    Started the login's window

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,5 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/auth-context";
+import { ChakraProvider, Image, Flex, Text, Input } from "@chakra-ui/react";
 
 function Login() {
   const navigate = useNavigate();
@@ -20,19 +21,47 @@ function Login() {
   }
 
   return (
-    <div>
-      <p>Você precisa estar logado para ver a página {from}</p>
-
-      <form onSubmit={handleSubmit}>
-        <label>
-          email: <input name="email" type="text" />
-        </label>{" "}
-        <label>
-          senha: <input name="password" type="password" />
-        </label>{" "}
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <ChakraProvider>
+      <Flex
+        backgroundImage={"./backgroundDog.png"}
+        height="262px"
+        maxW={"100vw"}
+        backgroundRepeat="round"
+        flexDirection={"column"}
+      >
+        <Image
+          src="./pawSymbol.png"
+          alt="paw"
+          width={"min-content"}
+          margin="46px 0 0 32px"
+        />
+        <Text
+          fontFamily={"Open Sans, sans-serif"}
+          color="white"
+          fontWeight="bold"
+          fontSize="36px"
+          width="302px"
+          height="98px"
+          lineHeight="49px"
+          margin="39.62px 0 24.88px 26px"
+        >
+          Comece agora.
+          <br />
+          Conecte-se já.
+        </Text>
+      </Flex>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            E-mail: <input name="email" type="text" />
+          </label>{" "}
+          <label>
+            Senha: <input name="password" type="password" />
+          </label>{" "}
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
+    </ChakraProvider>
   );
 }
 
